a8b1d6bccfa0d8260a3fbaaf924515aab6b92840
luphoria <60309933+luphoria@users.noreply.github.com>
2020-10-10 22:22:13 -0500

    borderpx from 1 to 2
    yeah so 1 px is a little too small even for me.
    also deleted "===" from layout and made monocle default.

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx = 1;		  /* border pixel of windows */
+static const unsigned int borderpx = 2;		  /* border pixel of windows */
 static const unsigned int snap = 32;		  /* snap pixel */
 static const unsigned int startmenusize = 30;		  /* snap pixel */
 static const unsigned int systraypinning = 0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -11,7 +11,7 @@ static const int systraypinningfailfirst = 1; /* 1: if pinning fails, display sy
 static const int showsystray = 1;			  /* 0 means no systray */
 static const int showbar = 1;				  /* 0 means no bar */
 static const int topbar = 1;				  /* 0 means bottom bar */
-static const char *fonts[] = {"Cantarell-Regular:size=12", "Fira Code Nerd Font:size=12"};
+static const char *fonts[] = {"Cantarell-Regular:size=11", "Fira Code Nerd Font:size=11"};
 
 static int barheight;
 static char xresourcesfont[30];
@@ -110,12 +110,11 @@ static const int decorhints  = 1;    /* 1 means respect decoration hints */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
+	{ "N/A",      monocle },
 	{ "+",        tile },    /* first entry is default */
 	{ "#",        grid },
 	{ "-",        NULL },    /* no layout function means floating behavior */
-	{ "N/A",      monocle },
 	{ "TTT",      bstack },
-	{ "===",      bstackhoriz },
 	{ NULL,       NULL },
 };
 
